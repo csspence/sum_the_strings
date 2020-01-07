9b29fc7527bb67e28c57bff45499b488fb1bf157
Crew Spence <csspence123@gmail.com>
2020-01-07 06:45:17 -0600

    Wrote working solution for sum_the_strings.

--- sum_the_strings.js
+++ sum_the_strings.js
@@ -5,3 +5,17 @@ Create a function that takes 2 positive integers in form of a string as an input
   sumStr("34", "5")   // should output "39"
 If either input is an empty string, consider it as zero.
 */
+
+const sumStr = (a,b) => {
+  let c = a;
+  let d = b;
+  
+  if(a === '') {
+    c = '0';
+  }
+  if(b === '') {
+    d = '0'
+  }
+
+  return (parseInt(c) + parseInt(d)).toString();
+}
